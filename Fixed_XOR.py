5e1a38aa6845db42947d36322f4de769f6750b07
Nick's Mac <Nick@Users-MacBook-Pro.local>
2018-02-13 16:30:19 -0600

    I've now added the XOR logic to the solution. Theoretically, the two string should now be XOR'ed together.Still need to store the new 1's and 0's in a new array, and then encode them in Hex values.

--- Fixed_XOR.py
+++ Fixed_XOR.py
@@ -1,11 +1,43 @@
+'''
+Created on Feb 13, 2018
 
-array = []
-for c in "1c01"
-	b = int(c,16)
-	print(b)
-	binary_value = bin(b)[2:].zfill(4)
-	array.append(binary_value)
+@author: Nick Govea
+'''
 
-print(array)
-s = "".join(array)
+
+array1 = []
+array2 = []
+for c in "1c":
+    b = int(c, 16)
+    binary_value = bin(b)[2:].zfill(4)
+    array1.append(binary_value)
+    
+for n in "68":
+    b = int(n, 16)
+    binary_value = bin(b)[2:].zfill(4)
+    array2.append(binary_value)
+
+    
+print(array1)
+print(array2)
+z = "".join(array2)
+s = "".join(array1)
 print(s)
+print(z)
+
+array3 = []
+for a,d in zip(s,z):
+      
+    if a=='0' and d == '0':
+        print("output=0")
+    if a=='0' and d =='1':
+        print("output=1")
+    if a=='1' and d=='0':
+        print("output=1")
+    if a=='1' and d=='1':
+        print("output=0")
+    
+
+
+                 
+print(array3) 
