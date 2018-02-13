b7fc2b827d4d7fdcd1e6547883efbd5435573e0f
Nick's Mac <Nick@Users-MacBook-Pro.local>
2018-02-13 14:04:02 -0600

    was finally able to covert the Hex string to binaty values and the binary values are now store in a new array. Next, I need to convert other Hex string and perform the XOR logic on the string.

--- Fixed_XOR.py
+++ Fixed_XOR.py
@@ -1,5 +1,11 @@
+
+array = []
 for c in "1c01"
 	b = int(c,16)
 	print(b)
 	binary_value = bin(b)[2:].zfill(4)
-	print(binary_value)
+	array.append(binary_value)
+
+print(array)
+s = "".join(array)
+print(s)
