5391b569503a5831299c03371146d8aa539f4082
Nick's Mac <Nick@Users-MBP.attlocal.net>
2018-02-13 19:51:31 -0600

    Added comments to the final version of the Fixed XOR challenge.

--- Fixed_XOR.py
+++ Fixed_XOR.py
@@ -5,6 +5,8 @@ Created on Feb 13, 2018
 '''
 
 
+#------ This section converts the Hex strings to binary values and stores the values in array1 and array2
+
 array1 = []
 array2 = []
 for c in "1c0111001f010100061a024b53535009181c":
@@ -17,13 +19,22 @@ for n in "686974207468652062756c6c277320657979":
     binary_value = bin(b)[2:].zfill(4)
     array2.append(binary_value)
 
-    
+
+
+#-------- This section prints out array1 and array2 to verify that binary values are correct.    
+
 print(array1)
 print(array2)
+
+
+#------- This section joins the values in the arrays to form one long string value in each array.
+
 z = "".join(array2)
 s = "".join(array1)
 print(s)
 print(z)
+
+#------- This section outlines the XOR logic, it cycles through each character in each array and stores the respective XOR value in array3.
 
 array3 = []
 for a,d in zip(s,z):
@@ -44,9 +55,12 @@ for a,d in zip(s,z):
     array3.append(n)
     
 
+#------- This section joins the values in array3 to form one long string of 1's and 0's
+
 string_array3 = "".join(array3)
 
 
+#----- This section takes the binary XOR string and converts it into a Hex string and prints it out.
                  
 print(string_array3) 
 print(hex(int(string_array3,2)))
